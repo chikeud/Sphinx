f639f179a7af621c8d18ad3bae1bdc74114372f7
emmanuel <eolaojo@yahoo.com>
2018-05-24 00:19:04 -0400

    auth-card progress

--- public/src/components/site/home.vue
+++ public/src/components/site/home.vue
@@ -1,13 +1,28 @@
 <template>
-  <m-card>
-    <m-typo-headline :level="2">
-      Title
-    </m-typo-headline>
+  <div id="home">
+    <div class="home-banner">
+      <div class="banner-content">
 
-    <m-typo-body :level="1">
+      </div>
 
-    </m-typo-body>
-  </m-card>
+      <div class="banner-auth">
+        <m-card class="auth-card">
+          <div class="auth-title">
+            Create Account
+          </div>
+
+          <div class="auth-user-type">
+            <button class="greyed-out">HOST</button>
+            <button class="active">STORE</button>
+          </div>
+
+          <div class="auth-form">
+            {{content}}
+          </div>
+        </m-card>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -15,12 +30,10 @@
   import Card from "material-components-vue/dist/card"
   import Button from "material-components-vue/dist/button"
   import Elevation from "material-components-vue/dist/elevation"
-  import Typography from "material-components-vue/dist/typography"
 
   Vue.use(Card);
   Vue.use(Button);
   Vue.use(Elevation);
-  Vue.use(Typography);
 
   export default {
     data(){
@@ -35,6 +48,63 @@
   @import "~material-components-vue/dist/card/styles";
   @import "~material-components-vue/dist/button/styles";
   @import "~material-components-vue/dist/elevation/styles";
-  @import "~material-components-vue/dist/Typography/styles";
 
+  #home{
+    font-family: "Montserrat", sans-serif ;
+    position: relative;
+    top: 130px;
+    margin: 0;
+    padding: 0;
+  }
+
+  .home-banner{
+    display: flex;
+    flex-direction: row;
+  }
+
+  .auth-card{
+    width: 330px;
+    padding: 20px 40px;
+    margin-right: 80px;
+  }
+
+  .banner-content{
+    flex: 3;
+  }
+
+  .banner-auth{
+    display: flex;
+    justify-content: space-between;
+    flex: 1;
+  }
+
+  .auth-title{
+    text-align: left;
+    font-size: 19px;
+    margin-bottom: 25px;
+  }
+
+  .auth-user-type{
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 20px;
+  }
+
+  .auth-user-type button{
+    border: none;
+    width: 115px;
+    height: 35px;
+    color: white;
+    font-size: 14px;
+    font-weight: bold;
+  }
+
+  .greyed-out{
+    background: #EBEFF1;
+    color: #CBD6DA !important;
+  }
+
+  .active{
+    background: #369FDA;
+  }
 </style>

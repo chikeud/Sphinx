355504174a12f8048c36de4212f08c567b720588
Karim Hassanali <karimhassanali@hotmail.com>
2018-06-01 01:34:23 -0400

    Test again with directories and images

---
+++ public/src/components/sites/nav.vue
@@ -0,0 +1,14 @@
+<template>
+  <top-app-bar></top-app-bar>
+</template>
+
+<script>
+  import vue from "vue"
+  import TopAppBar from "material-components-vue/dist/top-app-bar"
+
+  vue.use(TopAppBar)
+</script>
+
+<style>
+
+</style>

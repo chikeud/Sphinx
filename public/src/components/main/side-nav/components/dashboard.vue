aff22f74f3a5a42486e276129948eb980c876059
ChikeUdenze <chikeudenze@gmail.com>
2019-03-07 21:15:27 -0800

    initial dashboard styling

--- public/src/components/main/side-nav/components/dashboard.vue
+++ public/src/components/main/side-nav/components/dashboard.vue
@@ -1,10 +1,28 @@
 <template>
-  <div class="blah">
+  <div class="container">
     {{msg}}
   </div>
 </template>
 
 <script>
+  import Vue from "vue";
+  import Card from "material-components-vue/dist/card";
+  import Button from "material-components-vue/dist/button";
+  import TextField from "material-components-vue/dist/textfield";
+  import NotchedOutline from "material-components-vue/dist/notched-outline";
+  import FloatingLabel from "material-components-vue/dist/floating-label";
+  import Elevation from "material-components-vue/dist/elevation";
+  import Icon from "material-components-vue/dist/icon";
+
+
+  Vue.use(Card);
+  Vue.use(Button);
+  Vue.use(TextField);
+  Vue.use(NotchedOutline);
+  Vue.use(FloatingLabel);
+  Vue.use(Elevation);
+  Vue.use(Icon);
+
   export default {
     data(){
       return {
@@ -15,7 +33,11 @@
 </script>
 
 <style lang="scss">
-  .blah{
+  .left{
     text-align: center;
+  }
+  .container{
+    display: flex;
+    flex-direction: row;
   }
 </style>
